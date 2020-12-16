3407ac2ca8be288d08db4e38e073f02ef23c86df
steven cao <stevencao23@gmail.com>
2020-12-16 22:08:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+
+
+def loop_message_five_times (message)
+  5.times do 
+    puts (message)
+  end
+end
+loop_message_five_times ("\n Hello World.")
+
+
+def loop_message_n_times (string, integer)
+  counter = 0 
+  loop do
+    break if counter >= integer
+    puts (string)
+    counter += 1 
+  end
+end
+loop_message_n_times ("Hello Moon.") , (5)
+
+ array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+ def output_array
+  
